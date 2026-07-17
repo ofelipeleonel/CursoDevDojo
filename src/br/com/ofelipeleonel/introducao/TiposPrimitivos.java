f1ae54ac7515410afd4b2f5483e6c9bbf083038c
ofelipeleonel <felipeleonelmachado@hotmail.com>
2026-07-17 08:14:06 -0300

    Aula 02: Int

---
+++ src/br/com/ofelipeleonel/introducao/TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package br.com.ofelipeleonel.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, boolean
+        int idade = 10;
+        System.out.println("A idade é: " + idade);
+    }
+}
