3019e0d5fab0ee36aeb4946d51ad6f77e728782c
ofelipeleonel <felipeleonelmachado@hotmail.com>
2026-07-17 08:11:06 -0300

    Aula 01

--- src/CursoDevDojo.java
+++
@@ -1,5 +0,0 @@
-public class CursoDevDojo{
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/br/com/ofelipeleonel/introducao/OlaFelipe.java
@@ -0,0 +1,7 @@
+package br.com.ofelipeleonel.introducao;
+
+public class OlaFelipe {
+    public static void main(String[] args) {
+        System.out.println("Olá, Felipe!");
+    }
+}
