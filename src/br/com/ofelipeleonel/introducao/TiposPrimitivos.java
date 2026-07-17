9ccc0bde849abc0c0d5c8ac7067a424449474ac7
ofelipeleonel <felipeleonelmachado@hotmail.com>
2026-07-17 10:04:46 -0300

    Aula 02: Todos os tipos primitivos e tamanho

--- src/br/com/ofelipeleonel/introducao/TiposPrimitivos.java
+++ src/br/com/ofelipeleonel/introducao/TiposPrimitivos.java
@@ -2,8 +2,41 @@ package br.com.ofelipeleonel.introducao;
 
 public class TiposPrimitivos {
     public static void main(String[] args) {
-        //int, double, float, char, byte, boolean
-        int idade = 10;
-        System.out.println("A idade é: " + idade);
+        //int, float, double, long, short, char, byte, boolean
+
+        //Int: 4 bytes
+        int idadeInt = 10;
+        System.out.println("int: A idade é: " + idadeInt + ".");
+
+        //Float: 4 bytes
+        float salarioFloat = 2500;
+        System.out.println("float: O salário é: R$" + salarioFloat);
+
+        //Double: 8 bytes
+        double salarioDouble = 2000;
+        System.out.println("double: O salário é: R$" + salarioDouble);
+
+        //Long: 8 bytes
+        long numeroGrande = 1000000000;
+        System.out.println("long: O número grande é: " + numeroGrande);
+
+        //Short: 2 bytes
+        short idadeShort = 5;
+        System.out.println("short: A idade é: " + idadeShort);
+
+        //Char: 2 bytes (pois segue o padrão UTF-16)
+        char primeiraLetra = 'A';
+        System.out.println("char: A primeira letra do alfabeto é: " + primeiraLetra);
+
+        //Byte: 1 byte
+        byte temperatura = -15;
+        System.out.println("byte: A temperatura mínima de ontem foi " + temperatura + "ºC.");
+
+        //Boolean: 1 bit
+        boolean verdadeiro = true;
+        System.out.println("boolean: 1 + 1 = 1? " + verdadeiro);
+
+
+
     }
 }
